d273936fb23a861a4b9d1ed62172f43d9281bf3e
NickDekkersNL <nick.dekkers@student.uva.nl>
2022-03-27 12:13:59 +0200

    Code with errors for diagonalization

--- src/main.f90
+++ src/main.f90
@@ -1,16 +1,24 @@
 program main
-   use whatever
+   use Shooting
    implicit none
-
-   real c
-
-   c = calc(3.0)
-   print *, c
    
+   integer a, b, n
+   real h
+   real, allocatable :: x(:), s(:,:), v(:,:), l(:,:)
+   real(8), allocatable :: lambda(:,:), y(:)
+   a = -5
+   b = 5
+   n = 11
 
-   
+   allocate(s(n,n))
+   allocate(v(n,n))
+   allocate(lambda(n,n))
+   allocate(y(n))
 
-
-
+   !c = calc(3.0)
+   h = Integration_h(a, b, n) !1,00
+   call Integration_Grid(a,h,n,x) !-5,000 ... 5,0
+   call Determine_L(s, v, n,h, l)
+   call DetermineLambdaAndY(l, lambda, y)
 
 end program
--- src/module.f90
+++ src/module.f90
@@ -1,13 +1,70 @@
-module whatever
-    implicit none
-    contains
-    real function calc(x)
-    real, intent(in) :: x
-    calc = x +1
+module Shooting
+    use ExtraUsedSubroutines
+    use diagonalization
 
+    implicit none
+    save 
+    contains
+
+    real function Integration_h(a, b, n)
+        integer, intent(in) :: a, b, n
+        Integration_h = real((b - a) / real(n-1))
     end function
 
+    subroutine Integration_Grid(a, h, n, x)
+        real, intent(in) :: h
+        integer, intent(in) :: a, n
+        real, allocatable, intent(out) :: x(:)
+        integer i
+        allocate(x(int(n)))
 
+        do i = 1, int(n)
+            x(i) = a + (i-1)*h
+        enddo
+    end subroutine
 
+    subroutine Determine_L(s,v, n,h, l)
+        real :: s(:,:), v(:,:), h
+        real, allocatable, intent(out) :: l(:,:)
+        integer :: n
+        call S_value(s,n)
+        call V_value(v,n)
+        l = -(1.0 / (2*h**2))* s + v
+    end subroutine
+
+    subroutine DetermineLambdaAndY(l, lambda, y)
+        real, allocatable, intent(in) :: l(:,:)
+        real(8), intent(out) :: lambda(:,:), y(:)
+        call diagonalize(l, lambda, y)
+
+        
+    end subroutine
 end module
 
+
+module ExtraUsedSubroutines
+    implicit none
+    contains
+
+    subroutine S_value(s, n)
+        integer, intent(in) :: n
+        real, intent(inout) :: s(:,:)
+        integer :: i
+        
+        do i= 1, n
+            s(i,i) = -2.0
+            s(i,i+1)= -1.0
+            s(i+1,i) = -1.0
+        enddo
+    end subroutine
+
+    subroutine V_value(v, n)
+        integer, intent(in) :: n
+        real, intent(inout) :: v(:,:)
+        integer :: i
+
+        do i= 1,n
+            v(i,i) = 0.0
+        enddo
+    end subroutine
+end module
