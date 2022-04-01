d6f7370319cac6de825b3fbe68e0948a1b770570
NickDekkersNL <nick.dekkers@student.uva.nl>
2022-04-01 21:14:30 +0200

    Project done

--- src/main.f90
+++ src/main.f90
@@ -3,22 +3,28 @@ program main
    implicit none
    
    integer a, b, n
-   real h
-   real, allocatable :: x(:), s(:,:), v(:,:), l(:,:)
-   real(8), allocatable :: lambda(:,:), y(:)
-   a = -5
-   b = 5
-   n = 11
+   real(8) h
+   real(8), allocatable :: x(:), s(:,:), v(:,:), l(:,:), y_left(:), y_right(:)
+   real(8), allocatable :: y(:)
+   real(8) :: lambda, error, lambda_first
+   
+   a = -4
+   b = 4
+   n = 9
 
    allocate(s(n,n))
    allocate(v(n,n))
-   allocate(lambda(n,n))
+   lambda = 0
+
    allocate(y(n))
 
    !c = calc(3.0)
    h = Integration_h(a, b, n) !1,00
-   call Integration_Grid(a,h,n,x) !-5,000 ... 5,0
-   call Determine_L(s, v, n,h, l)
-   call DetermineLambdaAndY(l, lambda, y)
+   call Integration_Grid(a, h, n, x) !result: -5,000 ... 5,0
+   call Determine_L(s, v, n, h, l)
+   call DetermineLambdaAndY(l, y, lambda)
+
+   call Algorithm(y, h, y_left, y_right, v, lambda_first)
+   print *, lambda_first !The right lambda value
 
 end program
--- src/module.f90
+++ src/module.f90
@@ -1,20 +1,20 @@
 module Shooting
     use ExtraUsedSubroutines
-    use diagonalization
+    use Diagonalization
 
     implicit none
     save 
     contains
 
-    real function Integration_h(a, b, n)
+    real function Integration_h(a, b, n) !determine h
         integer, intent(in) :: a, b, n
         Integration_h = real((b - a) / real(n-1))
     end function
 
-    subroutine Integration_Grid(a, h, n, x)
-        real, intent(in) :: h
+    subroutine Integration_Grid(a, h, n, x) !determine the x integration grid
+        real(8), intent(in) :: h
         integer, intent(in) :: a, n
-        real, allocatable, intent(out) :: x(:)
+        real(8), allocatable, intent(out) :: x(:)
         integer i
         allocate(x(int(n)))
 
@@ -23,32 +23,56 @@ module Shooting
         enddo
     end subroutine
 
-    subroutine Determine_L(s,v, n,h, l)
-        real :: s(:,:), v(:,:), h
-        real, allocatable, intent(out) :: l(:,:)
+    subroutine Determine_L(s,v, n,h, l) !determine L(n)
+        real(8) :: s(:,:), v(:,:), h
+        real(8), allocatable, intent(out) :: l(:,:)
         integer :: n
         call S_value(s,n)
         call V_value(v,n)
-        l = -(1.0 / (2*h**2))* s + v
+        l = -(1.0 / (2.0*h**2))* s + v
     end subroutine
 
-    subroutine DetermineLambdaAndY(l, lambda, y)
-        real, allocatable, intent(in) :: l(:,:)
-        real(8), intent(out) :: lambda(:,:), y(:)
-        call diagonalize(l, lambda, y)
-
-        
+    subroutine DetermineLambdaAndY(l, y, lambda) !use diagonalization to determine lambda(n) and y(n)
+        real(8), allocatable, intent(in) :: l(:,:)
+        real(8), intent(out) :: lambda, y(:)
+        real(8), allocatable:: lambda_vector(:), y_vector(:,:)
+        allocate(lambda_vector(size(l(:,1))))
+        allocate(y_vector(size(lambda_vector),size(lambda_vector)))
+        call diagonalize(l, y_vector, lambda_vector)
+        y = y_vector(:,1)
+        lambda = lambda_vector(1)
     end subroutine
+
+
+    subroutine Algorithm(y, h, y_left, y_right, v, lambda_first)
+        real(8), intent(in) :: h, v
+        real(8), allocatable, intent(out) :: y_left(:), y_right(:), y(:), lambda_first
+
+        real(8):: y_left_accent, y_right_accent, lambda_after, error
+        integer i
+
+        error = 0.001 !tolerance level for lambda_after
+
+        do i = 1, 100
+            call ShootingMethod(y, h, y_left, y_right, v)
+            call NewLambda(h, y, y_left, y_right, y_left_accent, y_right_accent, lambda_first, lambda_after)
+            if ((lambda_after-lambda_first)<error)
+                exit
+            lambda_first = lambda_after
+        enddo
+    end subroutine
+
 end module
 
 
 module ExtraUsedSubroutines
+    use integration_module
     implicit none
     contains
 
-    subroutine S_value(s, n)
+    subroutine S_value(s, n) !make S grid
         integer, intent(in) :: n
-        real, intent(inout) :: s(:,:)
+        real(8), intent(inout) :: s(:,:)
         integer :: i
         
         do i= 1, n
@@ -58,13 +82,62 @@ module ExtraUsedSubroutines
         enddo
     end subroutine
 
-    subroutine V_value(v, n)
+    subroutine V_value(v, n) !make V grid
         integer, intent(in) :: n
-        real, intent(inout) :: v(:,:)
+        real(8), intent(inout) :: v(:,:)
         integer :: i
 
         do i= 1,n
             v(i,i) = 0.0
         enddo
     end subroutine
+
+    subroutine NewLambda(h, y, y_left, y_right, y_left_accent, y_right_accent, lambda_first, lambda_after)
+        real(8), intent(in) :: h, y_left(:), y_right(:), y(:), lambda_first
+        real(8), intent(out) :: y_left_accent, y_right_accent, lambda_after
+
+        real(8) :: v_int_left, v_int_right, difference
+ 
+    
+        call derivative(h, y_left, y_left_accent)
+        call derivative(h, y_right, y_right_accent)
+
+        call Newton_cotes(y_left, h, 1, size(y_left), v_int_left) !use trapez
+        call Newton_cotes(y_right, h, 1, size(y_right), v_int_right)
+
+        difference = 0.5 * ((y_right_accent/y_right(size(y_right)) - y_left_accent/y_left(size(y_left)) * 
+        ((1/v_int_left)**2) + v_int_right/y_right(size(y_right)**2)))
+        lambda_after = lambda_first - difference
+
+
+    end subroutine
+
+    subroutine derivative(h, y, y_accent)
+        real(8), intent(in) :: h, y(:)
+        real(8), intent(out) :: y_accent !y derivative
+        y_accent = (y(size(y)) - y(size(y)-2)) /(2*h)
+    end subroutine
+
+
+    subroutine ShootingMethod(y, h,y_left,y_right, v)
+        real(8), intent(in) :: y(:), v(:,:), h
+        real(8), intent(out), allocatable :: y_left(:), y_right(:)
+
+        integer :: number, i
+
+        number = size(y)/2
+        allocate(y_left(number))
+        allocate(y_right(number))
+
+        y_left(1) = y(1) !First 2 left and right values are known
+        y_left(2) = y(2)
+        y_right(1) = y(size(y))
+        y_right(2) = y(size(y)-1)
+
+        do i = 3, number !first 2 values are known, size of y_left is number. So number-2 iterations needed
+            y_left(i) = -y_left(i-2) + 2*h**2 *(v(i,i) - (1/h**2)) * y_left(i+1)
+            y_right(i) = -y_right(i-2) + 2*h**2 *(v(i,i)- (1/h**2))* y_right(i+1)
+        enddo
+    end subroutine
+
 end module
